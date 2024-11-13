300b35e6585cd63a89f96f0d77a2d521523ce677
Axel Bouvier <axelbouvierm@gmail.com>
2024-11-12 21:28:12 -0300

    Creacion de destinos + modificacion. Se refleja en el front

--- gestionDest.js
+++ gestionDest.js
@@ -1,152 +1,38 @@
 const modal = document.querySelector("#modal");
 const form = document.querySelector("#form");
 
-let destinos = {
-    1: {
-        destino: "CARIBE",
-        precio: 1050,
-        oferta: true,
-        estado: true,
-        url: "../img/caribe.webp",
-        duracion: "7 días",
-        alojamiento: "6 noches en hotel 5 estrellas",
-        modalidad: "All inclusive",
-        vuelos: true,
-        traslado: true,
-        cupos: 34,
-        id: 1
-    },
-    2: {
-        destino: "EUROPA",
-        precio: 4500,
-        oferta: false,
-        estado: true,
-        url: "../img/europa.webp",
-        duracion: "15 días",
-        alojamiento: "14 noches en hotel 4 estrellas",
-        modalidad: "Desayuno incluido",
-        vuelos: true,
-        traslado: true,
-        cupos: 84,
-        id: 2
-    },
-    3: {
-        destino: "ASIA Y ÁFRICA",
-        precio: 7850,
-        oferta: true,
-        estado: true,
-        url: "../img/africa.webp",
-        duracion: "30 días",
-        alojamiento: "29 noches en hotel 5 estrellas",
-        modalidad: "No incluye comidas",
-        vuelos: true,
-        traslado: true,
-        cupos: 23,
-        id: 3
-    },
-    4: {
-        destino: "AMSTERDAM",
-        precio: 7850,
-        oferta: false,
-        estado: true,
-        url: "../img/amsterdam.webp",
-        duracion: "30 días",
-        alojamiento: "29 noches en hotel 5 estrellas",
-        modalidad: "No incluye comidas",
-        vuelos: true,
-        traslado: true,
-        cupos: 123,
-        id: 4
-    },
-    5: {
-        destino: "MARRUECOS",
-        precio: 7850,
-        oferta: true,
-        estado: true,
-        url: "../img/marruecos.webp",
-        duracion: "30 días",
-        alojamiento: "29 noches en hotel 5 estrellas",
-        modalidad: "No incluye comidas",
-        vuelos: true,
-        traslado: true,
-        cupos: 99,
-        id: 5
-    },
-    6: {
-        destino: "MENDOZA",
-        precio: 7850,
-        oferta: false,
-        estado: true,
-        url: "../img/mendoza.webp",
-        duracion: "30 días",
-        alojamiento: "29 noches en hotel 5 estrellas",
-        modalidad: "No incluye comidas",
-        vuelos: true,
-        traslado: true,
-        cupos: 27,
-        id: 6
-    },
-    7: {
-        destino: "RÍO DE JANEIRO",
-        precio: 7850,
-        oferta: true,
-        estado: false,
-        url: "../img/rio.webp",
-        duracion: "30 días",
-        alojamiento: "29 noches en hotel 5 estrellas",
-        modalidad: "No incluye comidas",
-        vuelos: true,
-        traslado: true,
-        cupos: 290,
-        id: 7
-    },
-    8: {
-        destino: "ROMA",
-        precio: 7850,
-        oferta: true,
-        estado: false,
-        url: "../img/roma.webp",
-        duracion: "30 días",
-        alojamiento: "29 noches en hotel 5 estrellas",
-        modalidad: "No incluye comidas",
-        vuelos: true,
-        traslado: true,
-        cupos: 54,
-        id: 8
-    },
-    9: {
-        destino: "RANDOM",
-        precio: 7850,
-        oferta: false,
-        estado: false,
-        url: "../img/viajeAmigos.webp",
-        duracion: "30 días",
-        alojamiento: "29 noches en hotel 5 estrellas",
-        modalidad: "No incluye comidas",
-        vuelos: true,
-        traslado: true,
-        cupos: 341,
-        id: 9
-    }
+let preLoadDest = {
+    1: new Dest("CARIBE", 1050, true, true, "../img/caribe.webp", "7 días", "6 noches en hotel 5 estrellas", "All inclusive", true, true, 34, 1),
+    2: new Dest("EUROPA", 4500, false, true, "../img/europa.webp", "15 días", "14 noches en hotel 4 estrellas", "Desayuno incluido", true, true, 84, 2),
+    3: new Dest("ASIA Y ÁFRICA", 7850, true, true, "../img/africa.webp", "30 días", "29 noches en hotel 5 estrellas", "No incluye comidas", true, true, 23, 3),
+    4: new Dest("AMSTERDAM", 7850, false, true, "../img/amsterdam.webp", "30 días", "29 noches en hotel 5 estrellas", "No incluye comidas", true, true, 123, 4),
+    5: new Dest("MARRUECOS", 7850, true, true, "../img/marruecos.webp", "30 días", "29 noches en hotel 5 estrellas", "No incluye comidas", true, true, 99, 5),
+    6: new Dest("MENDOZA", 7850, false, true, "../img/mendoza.webp", "30 días", "29 noches en hotel 5 estrellas", "No incluye comidas", true, true, 27, 6),
+    7: new Dest("RÍO DE JANEIRO", 7850, true, false, "../img/rio.webp", "30 días", "29 noches en hotel 5 estrellas", "No incluye comidas", true, true, 290, 7),
+    8: new Dest("ROMA", 7850, true, false, "../img/roma.webp", "30 días", "29 noches en hotel 5 estrellas", "No incluye comidas", true, true, 54, 8),
+    9: new Dest("RANDOM", 7850, false, false, "../img/viajeAmigos.webp", "30 días", "29 noches en hotel 5 estrellas", "No incluye comidas", true, true, 341, 9),
 }
+
 window.addEventListener('DOMContentLoaded', addDest);
 
 function addDest() {
-    localStorage.setItem('destinos', JSON.stringify(destinos))
+    localStorage.setItem('destinos', JSON.stringify(preLoadDest))
+    let destinos = JSON.parse(localStorage.getItem('destinos'))
+    console.log(destinos)
     let tabla = document.querySelector('#tablaDestinos tbody');
     tabla.innerHTML = ''
-    // Iterar sobre las claves del objeto destinos
+    // Iterar sobre las key del objeto destinos
     Object.keys(destinos).forEach(key => {
         let nuevaFila = document.createElement("tr");
         nuevaFila.classList.add("open-modal");
 
-        let oferta = destinos[key]['oferta'] ? 'SI' : 'NO';
-        let estado = destinos[key]['estado'] ? 'Activo' : 'Inactivo';
+        let oferta = destinos[key]['offer'] ? 'SI' : 'NO';
+        let estado = destinos[key]['state'] ? 'Activo' : 'Inactivo';
 
         nuevaFila.innerHTML = `
             <td>${destinos[key]['id']}</td>
-            <td>${destinos[key]['destino']}</td>
-            <td>${destinos[key]['precio']}</td>
+            <td>${destinos[key]['dest']}</td>
+            <td>${destinos[key]['price']}</td>
             <td>${oferta}</td>
             <td id="estado">${estado}</td>
         `;
@@ -160,17 +46,17 @@ function addDest() {
         row.addEventListener('click', function () {
 
             let id = this.cells[0].textContent
-            document.getElementById("inputDestino").value = destinos[+id]['destino'];
-            document.getElementById("inputPrecio").value = destinos[+id]['precio'];
-            document.getElementById("inputOferta").value = destinos[+id]['oferta'] ? 'Si' : 'No';
-            document.getElementById("inputEstado").value = destinos[+id]['estado'] ? 'Activo' : 'Inactivo';
+            document.getElementById("inputDestino").value = destinos[+id]['dest'];
+            document.getElementById("inputPrecio").value = destinos[+id]['price'];
+            document.getElementById("inputOferta").value = destinos[+id]['offer'] ? 'Si' : 'No';
+            document.getElementById("inputEstado").value = destinos[+id]['state'] ? 'Activo' : 'Inactivo';
             document.getElementById("inputFoto").value = destinos[+id]['url'];
-            document.getElementById("inputDuracion").value = destinos[+id]['duracion'];
-            document.getElementById("inputAlojamiento").value = destinos[+id]['alojamiento'];
-            document.getElementById("inputModalidad").value = destinos[+id]['modalidad'];
-            document.getElementById("inputVuelos").value = destinos[+id]['vuelos'] ? 'Si' : 'No';
-            document.getElementById("inputTraslado").value = destinos[+id]['traslado'] ? 'Si' : 'No';
-            document.getElementById("inputCupos").value = destinos[+id]['cupos']
+            document.getElementById("inputDuracion").value = destinos[+id]['duration'];
+            document.getElementById("inputAlojamiento").value = destinos[+id]['hotel'];
+            document.getElementById("inputModalidad").value = destinos[+id]['modality'];
+            document.getElementById("inputVuelos").value = destinos[+id]['flights'] ? 'Si' : 'No';
+            document.getElementById("inputTraslado").value = destinos[+id]['transfer'] ? 'Si' : 'No';
+            document.getElementById("inputCupos").value = destinos[+id]['quotas']
             document.getElementById("inputID").value = destinos[+id]['id']
 
             modal.showModal();
@@ -191,10 +77,12 @@ modal.addEventListener('click', function (event) {
     }
 });
 
+let nuevoDestino = false
 const buttonAdd = document.querySelectorAll('.open-modal-add');
 
 buttonAdd.forEach(buttonAdd => {
     buttonAdd.addEventListener('click', () => {
+        nuevoDestino = true
         document.getElementById("inputDestino").value = ""
         document.getElementById("inputPrecio").value = ""
         document.getElementById("inputOferta").value = ""
@@ -206,28 +94,50 @@ buttonAdd.forEach(buttonAdd => {
         document.getElementById("inputVuelos").value = ""
         document.getElementById("inputTraslado").value = ""
         document.getElementById("inputCupos").value = ""
-        document.getElementById("inputID").value = ""
+        let id = Object.keys(preLoadDest).length + 1
+        document.getElementById("inputID").value = id
         modal.showModal();
     });
 });
 
 let buttonSendMod = document.querySelector('#send')
 buttonSendMod.addEventListener('click', function (event) {
+    /* let destinos = JSON.parse(localStorage.getItem('destinos')) */
 
-    let id = document.getElementById("inputID").value
-    destinos[+id]['destino'] = document.getElementById("inputDestino").value
-    destinos[+id]['precio'] = document.getElementById("inputPrecio").value
-    destinos[+id]['oferta'] = document.getElementById("inputOferta").value == 'Si' ? true : false
-    destinos[+id]['estado'] = document.getElementById("inputEstado").value == 'Activo' ? true : false
-    destinos[+id]['url'] = document.getElementById("inputFoto").value
-    destinos[+id]['duracion'] = document.getElementById("inputDuracion").value
-    destinos[+id]['alojamiento'] = document.getElementById("inputAlojamiento").value
-    destinos[+id]['modalidad'] = document.getElementById("inputModalidad").value
-    destinos[+id]['vuelos'] = document.getElementById("inputVuelos").value == 'Si' ? true : false
-    destinos[+id]['traslado'] = document.getElementById("inputTraslado").value == 'Si' ? true : false
-    destinos[+id]['cupos'] = document.getElementById("inputCupos").value
+    if (!nuevoDestino) {
+        let id = document.getElementById("inputID").value
+        preLoadDest[+id]['dest'] = document.getElementById("inputDestino").value
+        preLoadDest[+id]['price'] = document.getElementById("inputPrecio").value
+        preLoadDest[+id]['offer'] = document.getElementById("inputOferta").value == 'Si' ? true : false
+        preLoadDest[+id]['state'] = document.getElementById("inputEstado").value == 'Activo' ? true : false
+        preLoadDest[+id]['url'] = document.getElementById("inputFoto").value
+        preLoadDest[+id]['duration'] = document.getElementById("inputDuracion").value
+        preLoadDest[+id]['hotel'] = document.getElementById("inputAlojamiento").value
+        preLoadDest[+id]['modality'] = document.getElementById("inputModalidad").value
+        preLoadDest[+id]['flights'] = document.getElementById("inputVuelos").value == 'Si' ? true : false
+        preLoadDest[+id]['transfer'] = document.getElementById("inputTraslado").value == 'Si' ? true : false
+        preLoadDest[+id]['quotas'] = document.getElementById("inputCupos").value
+    } else {
+        let id = document.getElementById("inputID").value
 
+        preLoadDest[id] = new Dest(
+            document.getElementById("inputDestino").value,
+            document.getElementById("inputPrecio").value,
+            document.getElementById("inputOferta").value == 'Si' ? true : false,
+            document.getElementById("inputEstado").value == 'Activo' ? true : false,
+            document.getElementById("inputFoto").value,
+            document.getElementById("inputDuracion").value,
+            document.getElementById("inputAlojamiento").value,
+            document.getElementById("inputModalidad").value,
+            document.getElementById("inputVuelos").value == 'Si' ? true : false,
+            document.getElementById("inputTraslado").value == 'Si' ? true : false,
+            document.getElementById("inputCupos").value,
+            `${id}`)
+
+        nuevoDestino = false
+    }
     addDest()
     modal.close()
 })
+
 
--- index.js
+++ index.js
@@ -2,26 +2,65 @@ let buttons = document.querySelectorAll(".open-modal"); // Seleccionar todos los
 let modal = document.querySelector("#modal");
 let form = document.querySelector("#form");
 
+// Carga los destinos al iniciar la web
+actualizarDestinos();
+
+function actualizarDestinos() {
+    let div = document.querySelector('#articlesPaquetes')
+    div.innerHTML = ''
+    let destinos = JSON.parse(localStorage.getItem('destinos'))
+
+    Object.keys(destinos).forEach(key => {
+
+        if (destinos[key]["state"] == true) {
+            let oferta = destinos[key]["offer"] ? `<p class="oferta">Oferta</p>` : `<p class="oferta hidden">Oferta</p>`
+            let vuelos = destinos[key]["flights"] ? 'Incluye vuelos ida y vuelta' : 'No incluye vuelos ida y vuelta'
+            let traslado = destinos[key]["transfer"] ? 'Incluye traslados al aeropuerto' : 'No incluye traslados al aeropuerto'
+
+                div.innerHTML += `<article>
+                    <div class="paquetes-img">
+                        ${oferta}
+                        <img src=${destinos[key]["url"]} alt="Imagen del Caribe">
+                    </div>
+                    <div class="paquetes-info">
+                        <h3><strong>${destinos[key]["dest"]}</strong></h3>
+                        <p>${destinos[key]["price"]} <small>USD</small></p>
+                        <ul>
+                            <li>Duración: ${destinos[key]["duration"]}</li>
+                            <li>Alojamiento: ${destinos[key]["hotel"]}</li>
+                            <li>Modalidad: ${destinos[key]["modality"]}</li>
+                            <li>${vuelos}</li>
+                            <li>${traslado}</li>
+                        </ul>
+                    </div>
+                    <div id="aCont">
+                        <button class="open-modal" id-destino=${destinos[key]["id"]} href="#">RESERVAR</button>
+                    </div>
+                </article>`
+        }
+    })
+}
+
 // Abrir el modal al hacer clic en cualquiera de los botones
 buttons.forEach(button => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         modal.showModal();
     });
 });
 
 // Cerrar el modal al enviar el formulario
-form.addEventListener('submit', function() {
+form.addEventListener('submit', function () {
     modal.close();
 });
 
 // Cerrar el modal al hacer clic fuera de él
-modal.addEventListener('click', function(event) {
+modal.addEventListener('click', function (event) {
     if (event.target === modal) {
         modal.close();
     }
 })
 
-function login() { 
+function login() {
     let user = document.querySelector('#inputUserName').value
     let pwd = document.querySelector('#inputPassword').value
 
@@ -32,3 +71,11 @@ function login() {
     }
 }
 
+
+// Evento para escuchar los cambios en el storage
+window.addEventListener('storage', (event) => {
+    // Actualizar los destinos ante cambios
+    if (event.key === 'destinos') {
+        actualizarDestinos()
+    }
+});
