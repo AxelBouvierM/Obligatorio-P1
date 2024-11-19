dcc997c0ed5ef20d8eafd41941850e25242a2a7a
maurimontero09 <mauriciomonterolasiu@gmail.com>
2024-11-19 12:47:29 -0300

    validaciones de reservas - almost done

--- gestionReserv.js
+++ gestionReserv.js
@@ -38,6 +38,7 @@ function verifyReserv(event) {
     let reserv = window.Sistema.getItemToLocalStorage('reservations')
     let dest = window.Sistema.getItemToLocalStorage('destinos')
     let index = 0;
+    console.log(reserv)
 
     while (index < reserv.length) {
         let element = reserv[index];
@@ -47,15 +48,39 @@ function verifyReserv(event) {
                 element.state = 'Rechazada'
                 break
             }
-            /* if(millas o efectivo alcanzan?) estado rachazad break*/
-            /* else {
+            // Validar si el usuario tiene suficiente credito, sino rechazar la reserva
+            if (element.payment == 'Efectivo' && element.cant * dest[element.destID.split('_')[2]].price > window.Sistema.user.credit) {
+                element.state = 'Rechazada'
+                break 
+            }
+            // Validar si el usuario tiene suficientes millas sino descontar las que tenga disponibles
+            if (element.payment == "Millas" && element.cant * dest[element.destID.split('_')[2]].price > window.Sistema.user.miles) {
+                let totalPrice = element.cant * dest[element.destID.split('_')[2]].price
+                totalPrice -= window.Sistema.user.miles
+                if (window.Sistema.user.miles == 0) {
+                    window.Sistema.user.credit -= totalPrice
+                    element.state = 'Aprobada'
+                    break 
+                } 
+            // Si tiene suficientes millas, se descuenta el total del viaje de sus millas
+            } else {
+                window.Sistema.user.miles -= element.cant * dest[element.destID.split('_')[2]].price
                 element.state = 'Aprobada'
-            } */// cambiar status a rechazada sin cupos disponibles.
+                break 
+            }
+            //Si se cumplen las condiciones, se aprueba la reserva
             element.state = 'Aprobada'
+            //Si se aprueba la reserva, se descuenta la cantidad de cupos disponibles
+            dest.quotas -= element.cant
+            //Si la cantidad de cupos disponibles es 0, se pausa el destino
+            if (dest.quotas == 0) {
+                dest.state = 'Pausado'
+            }
             break;
         }
         index++;
     }
+    
     window.Sistema.pushItemToLocalStorage('reservations', reserv)
     window.Sistema.reservations = reserv
     refreshReservations()
